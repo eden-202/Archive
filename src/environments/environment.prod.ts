322a66e9827a509525edf9487468c9e0eabdf2fa
eden <edenyoalashet@gmail.com>
2025-01-17 12:41:07 +0200

    Initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  name: "prod",
+  firebase: {
+    apiKey: "AIzaSyBVxX4RJQmq7Fv3oeL1Lw1T8LDcBrELrpM",
+    authDomain: "onoflight-fb99a.firebaseapp.com",
+    databaseURL: "https://onoflight-fb99a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "onoflight-fb99a",
+    storageBucket: "onoflight-fb99a.firebasestorage.app",
+    messagingSenderId: "306963294931",
+    appId: "1:306963294931:web:807220a6d32ed8ee3b8226",
+  },
+};
